71e6a16afdb51f6e2eb672d364e3f615516d4bc6
ltb-bossuyt <luongthibien.bossuyt@gmail.com>
2019-02-22 11:09:17 +0100

    Correctif bug HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -42,62 +42,16 @@ export class HomeScreen extends BaseScreen {
                     style={{justifyContent: 'center',alignItems: 'center',flex: 1, width: "100%", height: "100%"}}
                     source={require('../assets/bg_foot.jpg')}>
 
-                    /*<View style={styles.contentView}>
+                    <View style={styles.contentView}>
                         <View style={styles.form}>
-                            <Text style={styles.text}>Vos points</Text>
-                            <Text style={styles.text}>{getUser().score}</Text>
+                            <Text style={styles.text}>En attente d'instructions de la table</Text>
                         </View>
-                    </View>*/
+                    </View>
                 </ImageBackground>
             </View>
         );
     }
 }
-
-
-
-/* <Button
-     buttonStyle={styles.buttonStyle}
-     title='Reset Server'
-     iconRight
-     onPress={() => {
-         reset();
-     }}
- />
- <Button
-     buttonStyle={styles.buttonStyle}
-     title='Question collectif v2'
-     iconRight
-     onPress={() => {
-         //send('question-collectif-request-v2', 'request');
-         navigate('QuestionCollectifV2');
-     }}
- />
-
- <Button
-     buttonStyle={styles.buttonStyle}
-     title='Question collectif (parralèle)'
-     iconRight
-     onPress={() => {
-         //send('question-collectif-request-v2', 'request');
-         navigate('QuestionCollectifParrallel');
-     }}
- />
-
-
- <Button
-     buttonStyle={styles.buttonStyle}
-     title='QuestionCollectif'
-     iconRight
-     onPress={() => {
-         send('question-collectif-request', 'request');
-         //navigate('QuestionCollectif');
-     }}
- />
-
- <Button buttonStyle={styles.buttonStyle} title={"Question simple"} onPress={() => {
-     navigate("Wait");
- }}/>*/
 
 const styles = StyleSheet.create({
     container: {
